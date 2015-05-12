1347927e400275659a3b40a19294d068f70e2860
Adam Thompson <adam.thompson@uwaterloo.ca>
2015-05-12 19:59:17 -0400

    reset local debug variable

--- includes/php/global-head.php
+++ includes/php/global-head.php
@@ -1,14 +1,13 @@
 <?php $page_title = ucfirst($page_id);
 	include_once('mySchoolTime.php');
 
-	$DEBUG = false;
+	$DEBUG = true;
 	if ($DEBUG) {
 	 	$root_path = 'http://localhost:8888/adam-thompson-4';
 	 } 
 	 else {
 	 	$root_path = 'http://adam-thompson.ca';
 	 }
-	 $allow_url_include=0;
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
 	"http://www.w3.org/TR/html4/strict.dtd">
