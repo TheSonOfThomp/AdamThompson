3f166e2106447f606091eab31bf394ac6d09968b
Adam Thompson <adam.thompson@uwaterloo.ca>
2016-01-11 17:37:38 -0500

    addet git to footer

--- includes/php/footer.php
+++ includes/php/footer.php
@@ -8,7 +8,7 @@
 			<li class="animate social-media"><a id="twitter" href="https://twitter.com/TheSonOfThomp" target="_blank"></a></li>
 			<li class="animate social-media"><a id="linkedin" href="https://ca.linkedin.com/in/adammthompson" target="_blank"></a></li>
 			<li class="animate social-media hide-mobile"><a id="behance" href="https://www.behance.net/TheSonOfThomp" target="_blank"></a></li>
-			<li class="animate social-media hide-mobile"><a id="codepen" href="http://codepen.io/TheSonOfThomp/" target="_blank"></a></li>
+			<li class="animate social-media hide-mobile"><a id="git" href="http://github.com/Adam93MT" target="_blank"></a></li>
 		</ul>
 	</div>
 </footer>
