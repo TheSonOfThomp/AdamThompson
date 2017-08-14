aac218f5e1bac12ce04ff3a64e2bd591c54a786a
adam <adam@adamthompson.ca>
2017-08-14 13:05:29 -0400

    Edited Bio text

--- includes/php/bio.php
+++ includes/php/bio.php
@@ -4,10 +4,10 @@
 			<img alt="Portrait taken by Natasha Choo" src="includes/image_assets/Adam_web.jpg">
 		</div>
 		<span class="bio-text" id="about">
-			<p>Hey, I’m a <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Designer with an Engineering degree</a> from the University of Waterloo.
+			<p>I’m a <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Designer with an Engineering degree</a> from the University of Waterloo.
 			<p>I’m a big-picture problem solver—always looking for the best solution to the right problem.</p>
 			<p>I have a big passion for music, travel, and healthcare.</p>
-			<p>Let's work together and make something great!</p>
+			<!-- <p>Let's work together and make something great!</p> -->
 		</span>
 	<ul class="social-list dark" id="social">
 		<li class="social-media tooltip">
