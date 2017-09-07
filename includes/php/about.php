bbffd976be4f85ba7e60eafb72994b62bdc71495
adam <adam@adamthompson.ca>
2017-09-07 15:42:46 -0400

    Edited Bio & about

--- includes/php/about.php
+++ includes/php/about.php
@@ -6,7 +6,7 @@
 				<img alt="Portrait taken by Natasha Choo" src="includes/image_assets/Adam_web.jpg">
 			</div>
 			<p class="about-text">
-			Trained in <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Systems Design Engineering</a>, I'm a multifaceted product designer, employing a thoughtful, methodical process to product design. While I'm more experienced with software, I don't limit myself to the medium. I've applied this design process at Noom, KnowRoaming, Sony and Zynga, and have won the <a target="_blank" href="https://uwaterloo.ca/engineering/news/six-teams-win-10000-each-annual-esch-awards">Norman Esch Entrepreneurship award</a> with Chameleon Hearing Protection.
+			With a degree in <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Systems Design Engineering</a>, I'm a multifaceted product designer, bringing a holistic process to product design. While I have more experience in software design, I'm not limited to the medium. I've designed at Noom, KnowRoaming, Sony and Zynga, and have won the <a target="_blank" href="https://uwaterloo.ca/engineering/news/six-teams-win-10000-each-annual-esch-awards">Norman Esch Entrepreneurship award</a> with Chameleon Hearing Protection.
 			<br/><br/>
 			When I'm not making products, I enjoy making music. In April 2017 I had the amazing opportunity to sing at the Beacon Theatre in New York with <a target="_blank" href="http://www.570news.com/2017/04/05/uw-capella-group-shot-world-title/"> the Water Boys</a> for the ICCA finals.
 			</p>	
@@ -19,28 +19,6 @@
 				<li><a href="">Chameleon wins University of Waterloo's Norman Esch Entrepreneurship award</a></li>
 			</ul>
 		</div> -->
-
-<ul class="social-list body dark" id="social">
-		<li class="social-media tooltip">
-			<a id="email" href="mailto:adam.m.thompson@icloud.com?Subject=Hey!" target="_blank"></a>
-			<span class="tooltiptext">Email</span>
-		</li>
-		<li class="social-media tooltip">
-			<a id="twitter" href="https://www.twitter.com/TheSonOfThomp" target="_blank"></a>
-			<span class="tooltiptext">Twitter</span>
-		</li>
-		<li class="social-media tooltip">
-			<a id="linkedin" href="https://ca.linkedin.com/in/adammthompson" target="_blank"></a>
-			<span class="tooltiptext">LinkedIn</span>
-		</li>
-		<li class="social-media tooltip hide-mobile">
-			<a id="git" href="http://www.github.com/Adam93MT" target="_blank"></a>
-			<span class="tooltiptext">GitHub</span>
-		</li>
-		<li class="social-media tooltip hide-mobile">
-			<a id="instagram" href="https://www.instagram.com/TheSonOfThomp" target="_blank"></a>
-			<span class="tooltiptext">Instagram</span>
-		</li>
-	</ul>
+<?php include('social.php'); ?>
 	</div>
 </section>
--- includes/php/bio.php
+++ includes/php/bio.php
@@ -4,10 +4,10 @@
 			<img alt="Portrait taken by Natasha Choo" src="includes/image_assets/Adam_web.jpg">
 		</div> -->
 		<span class="bio-text" id="about">
-			<p>I’m a <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Designer with an Engineering degree</a> from the University of Waterloo.
-			<p>I’m a big-picture problem solver—always looking for the best solution to the right problem.</p>
-			<p>I have a big passion for music, travel, and healthcare.</p>
-			<!-- <p>Let's work together and make something great!</p> -->
+			<!-- <p>I’m a <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Designer with an Engineering degree</a> from the University of Waterloo.
+			</p> -->
+			<p>I design products—from the initial problem and requirements, to the final interface and experience, and all that's in between.
+			I've designed at Sony, KnowRoaming, and Noom in Waterloo, Toronto and New York. Let's work together and make something great!</p>
 		</span>
 	</div>
 </section>
