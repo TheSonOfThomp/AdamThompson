18fc5e85c49c0aee6f2f1b79d5a4afb0ce1ba529
adam <adam@adamthompson.ca>
2017-09-07 16:03:41 -0400

    Fixed mobile & social style

--- includes/php/header.php
+++ includes/php/header.php
@@ -1,3 +1,6 @@
+<!-- <div style="position: absolute;left: 0;width: 48px;padding: 16px;">
+	<img src="<?php echo $root_path; ?>/includes/image_assets/silhouette_icn.jpg" style="width: 100%;border-radius: 50%;">
+</div> -->
 <header class="main-header">
 	<div class="header-contents">
 		<div class="title"><a href="<?php echo $root_path; ?>/">Adam Thompson <?php ($DEBUG); ?></a></div>
