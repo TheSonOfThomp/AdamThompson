ba95b37ac0beb4e9e56f8e5dba59386e4002fbc3
adam <adam@adamthompson.ca>
2017-09-18 17:34:55 -0400

    About and Blog Section Style changes
    Note: css is not properly compiled

--- includes/php/about.php
+++ includes/php/about.php
@@ -1,20 +1,23 @@
 <section class="about" id="about">
 	<div class="container">
-		<h1 class="section-header">About me</h1>
-		<div class="content flex" id="bio">
+		<!-- <h1 class="section-header">About me</h1> -->
+		<div class="content" id="bio">
 			<div class="bio-pic">
 				<img alt="Portrait taken by Natasha Choo" src="includes/image_assets/Adam_web.jpg">
 			</div>
-			<p class="about-text">
-			I'm a product designer, and recent grad from <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Systems Design Engineering</a> at the University of Waterloo. My multidisciplinary engineering background allows me to bring a holistic  process to everything I design. 
-			<br/><br/>
-			I've designed at Noom, KnowRoaming, Sony and Zynga from Toronto to New York, and won the <a target="_blank" href="https://uwaterloo.ca/engineering/news/six-teams-win-10000-each-annual-esch-awards">Norman Esch Entrepreneurship award</a> with Chameleon Hearing Protection.
-			<br/><br/>
-			When I'm not making products, I enjoy making music. In April 2017 I had the amazing opportunity to sing at the Beacon Theatre in New York with the <a target="_blank" href="http://www.570news.com/2017/04/05/uw-capella-group-shot-world-title/">Water Boys</a> for the ICCA finals.
-			</p>	
+			<div class="about-text">
+				<p>
+					I'm a product designer, and recent grad from <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Systems Design Engineering</a> at the University of Waterloo. This multidisciplinary engineering background allows me to bring a holistic process to everything I design. 
+				</p>
+				<p>
+					I have designed at Noom, KnowRoaming, Sony and Zynga in places like Toronto and New York, and won the <a target="_blank" href="https://uwaterloo.ca/engineering/news/six-teams-win-10000-each-annual-esch-awards">Norman Esch Entrepreneurship award</a> with Chameleon Hearing Protection.
+				</p>
+				<p>
+					When I'm not making products, I enjoy making music. In April 2017 I had the amazing opportunity to sing at the Beacon Theatre in New York with the <a target="_blank" href="http://www.570news.com/2017/04/05/uw-capella-group-shot-world-title/">Water Boys</a> for the ICCA finals.
+				</p>	
 		</div>
 
-<!-- 		<div class="subsection-header">Recent News</div>
+		<!-- <div class="subsection-header">Recent News</div>
 		<div class="content" id="achievements">
 			<ul>
 				<li><a href="">The Water Boys take 5th at ICCA finals in New York City</a></li>
