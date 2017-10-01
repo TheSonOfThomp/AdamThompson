f69b0c9c6c5a72625b6a06727beb27bafdb3b280
adam <adam@adamthompson.ca>
2017-10-01 17:44:58 -0400

    Added better contact button

--- includes/php/social.php
+++ includes/php/social.php
@@ -1,32 +1,35 @@
+<div class="social-container">
 	<ul class="social-list dark" id="social">
-		<li class="social-media tooltip">
+		<!-- <li class="social-media tooltip">
 			<a href="mailto:adam.m.thompson@icloud.com?Subject=Hey!" target="_blank">
 				<div class="social-logo" id="email"></div>
 				<span class="social-name">Email</span>
 			</a>
+		</li> -->
+		<li class="social-media tooltip hide-mobile">
+			<a href="https://www.instagram.com/TheSonOfThomp" target="_blank">
+				<img class="social-logo" id="instagram" src="<?php echo $root_path.'/includes/image_assets/social/instagram_dark.png' ?>" />
+				<span class="social-name">Instagram</span>
+			</a>
 		</li>
 		<li class="social-media tooltip">
 			<a href="https://www.twitter.com/TheSonOfThomp" target="_blank">
-				<div class="social-logo" id="twitter"></div>
+				<img class="social-logo" id="twitter" src="<?php echo $root_path.'/includes/image_assets/social/twitter_dark.png' ?>" />
 				<span class="social-name">Twitter</span>
 			</a>
 		</li>
 		<li class="social-media tooltip">
 			<a href="https://ca.linkedin.com/in/adammthompson" target="_blank">
-				<div class="social-logo" id="linkedin"></div>
+				<img class="social-logo" id="linkedin" src="<?php echo $root_path.'/includes/image_assets/social/linkedin_dark.png' ?>" />
 				<span class="social-name">LinkedIn</span>
 			</a>
 		</li>
 		<li class="social-media tooltip hide-mobile">
 			<a href="http://www.github.com/Adam93MT" target="_blank">
-				<div class="social-logo" id="git"></div>
+				<img class="social-logo" id="git" src="<?php echo $root_path.'/includes/image_assets/social/git_dark.png' ?>" />
 				<span class="social-name">GitHub</span>
 			</a>
 		</li>
-		<li class="social-media tooltip hide-mobile">
-			<a href="https://www.instagram.com/TheSonOfThomp" target="_blank">
-				<div class="social-logo" id="instagram"></div>
-				<span class="social-name">Instagram</span>
-			</a>
-		</li>
 	</ul>
+	<a class="button large" href="mailto:adam.m.thompson@icloud.com?Subject=Hi!">Get in touch!<i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+</div>
