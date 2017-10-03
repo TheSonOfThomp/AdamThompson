ae200c1730ec4d7b57ad39fafd50d622422be491
adam <adam@adamthompson.ca>
2017-10-02 23:27:42 -0400

    More edits

--- includes/php/social.php
+++ includes/php/social.php
@@ -1,12 +1,6 @@
 <div class="social-container">
 	<ul class="social-list dark" id="social">
-		<!-- <li class="social-media tooltip">
-			<a href="mailto:adam.m.thompson@icloud.com?Subject=Hey!" target="_blank">
-				<div class="social-logo" id="email"></div>
-				<span class="social-name">Email</span>
-			</a>
-		</li> -->
-		<li class="social-media tooltip hide-mobile">
+		<li class="social-media tooltip">
 			<a href="https://www.instagram.com/TheSonOfThomp" target="_blank">
 				<img class="social-logo" id="instagram" src="<?php echo $root_path.'/includes/image_assets/social/instagram_dark.png' ?>" />
 				<span class="social-name">Instagram</span>
