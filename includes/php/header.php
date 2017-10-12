4a7afce35f8c289da5f0ddb243c7d26070bb2a98
adam <adam@adamthompson.ca>
2017-10-12 14:04:15 -0400

    New header

--- includes/php/header.php
+++ includes/php/header.php
@@ -1,14 +1,25 @@
-<!-- <div style="position: absolute;left: 0;width: 48px;padding: 16px;">
-	<img src="<?php echo $root_path; ?>/includes/image_assets/silhouette_icn.jpg" style="width: 100%;border-radius: 50%;">
-</div> -->
 <header class="main-header">
 	<div class="header-contents">
 		<div class="title">
-			<a href="<?php echo $root_path; ?>/">Adam Thompson
-			</a>
-			<div class="tagline">
+			<a href="http://localhost:9999/AdamThompson/" class="name"><em>Adam</em> Thompson</a>
+			<ul>
+				<li><a href="#portfolio">Work</a></li>
+				<li><a href="#about">About</a></li>
+				<li><a href="mailto:adam.m.thompson@icloud.com">Contact</a><i class="fa fa-arrow-right" aria-hidden="true"></i></li>
+			</ul>
+			<!-- <span class="tagline">
 				<span class="tag">Product</span> Designer
+			</span> -->
+		</div>
+		<div class="hero">
+			<div class="headline">
+				Designer, Developer &amp; Researcher
 			</div>
+			<div class="bio-text">
+				<p>I make products for people—from researching the problem and requirements, to building the final experience and interface, and all that's in between.
+				</p>
+			</div>
+			<div class="bg-letter">UX</div>
 		</div>
 	</div>
 </header>
