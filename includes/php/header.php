a5e671cb93b00359ae01f21ee12d1b27f37e357c
adam <adam@adamthompson.ca>
2017-11-13 16:45:02 -0500

    Made header more responsive

--- includes/php/header.php
+++ includes/php/header.php
@@ -17,7 +17,7 @@
 				<p>I make products for people—from researching the problem and requirements, to building the final experience and interface, and all that's in between.
 				</p>
 			</div>
-			<div class="bg-letter split-ux">
+			<div class="bg-letter">
 				<span id="u">U</span><span id="x">X</span>
 			</div>
 		</div>
