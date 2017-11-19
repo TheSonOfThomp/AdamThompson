16c17c5c1359c22f66bbe9be97d423a490d10069
adam <adam@adamthompson.ca>
2017-11-18 22:19:09 -0500

    Portfolio page tweaks

--- includes/php/navbar.php
+++ includes/php/navbar.php
@@ -1,5 +1,4 @@
-	<header class="main-header green-bg">
-	<div class="header-contents">
+<div class="navbar">
 		<div class="title">
 			<div style=" margin-bottom: -10px">
 				<a href="<?php echo $root_path ?>" class="name">
@@ -7,7 +6,6 @@
 					<img src="<?php echo $root_path."/includes/image_assets/monogram-white.svg" ?>">
 				</a>
 			</div>
-			
 			<div class="hamburger-drawer">
 				<div class="hamburger-icon"><a><i class="fa fa-bars" aria-hidden="true"></i><i class="fa fa-times" aria-hidden="true"></i></a></div>
 				<ul class="nav-list">
@@ -18,17 +16,3 @@
 			</div>
 		</div>	
 	</div>
-	<div class="hero">
-			<div class="hero-name">Adam Thompson</div>
-			<div class="tagline">
-				Designer. Developer. Researcher.
-			</div>
-			<!-- <div class="bio-text">
-				<p>I make products for people—from researching the problem and requirements, to building the final experience and interface, and all that's in between.
-				</p>
-			</div>-->			
-			<div class="bg-letter">
-				<span id="u">U</span><span id="x">X</span>
-			</div>
-		</div>
-</header>
