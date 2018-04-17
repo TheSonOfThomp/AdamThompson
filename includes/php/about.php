b3abad05cd3aacb23880c50ed9febba9d4a8f63d
adam <adam@adamthompson.ca>
2018-04-17 11:58:36 -0400

    Edited About text and some formatting

--- includes/php/about.php
+++ includes/php/about.php
@@ -5,17 +5,17 @@
 	<div class="container">
 		<div class="content" id="bio">
 			<div class="bio-pic">
-				<img alt="Portrait taken by Natasha Choo" src="includes/image_assets/Adam_web.jpg">
+				<img alt="Portrait by Natasha Choo 2017" src="includes/image_assets/Adam_web.jpg">
 			</div>
 			<div class="about-text">
 				<p>
-					I'm a product designer, and recent grad from <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Systems Design Engineering</a> at the University of Waterloo, with a minor in Cognitive Science. My multidisciplinary background allows me to bring a holistic process to everything I design. 
+					I'm a user researcher, product designer, and graduate from <a target="_blank" href="https://uwaterloo.ca/systems-design-engineering/about-systems-design-engineering/what-systems-design-engineering">Systems Design Engineering</a> at the University of Waterloo, with a minor in Cognitive Science. My multidisciplinary engineering background allows me to bring a holistic process to everything I design. 
 				</p>
 				<p>
-					I've designed at Noom, KnowRoaming, Sony and Zynga, conducting user research, developing user flows and wireframes, refining UIs, and testing prototypes. I recently won the <a target="_blank" href="https://uwaterloo.ca/engineering/news/six-teams-win-10000-each-annual-esch-awards">Norman Esch Entrepreneurship award</a> with Chameleon Hearing Protection.
+					I've worked at MUSIC Group, Noom, KnowRoaming, Sony and Zynga, conducting market and customer research, developing user flows and wireframes, refining UIs, and testing prototypes. My company Chameleon Hearing Protection won the <a target="_blank" href="https://uwaterloo.ca/engineering/news/six-teams-win-10000-each-annual-esch-awards">Norman Esch Entrepreneurship award</a> in March 2017, and was a semi-finalist for AC JumpStart.
 				</p>
 				<p>
-					When I'm not making products, I enjoy making music. In April 2017 I had the amazing opportunity to sing at the Beacon Theatre in New York with the <a target="_blank" href="http://www.570news.com/2017/04/05/uw-capella-group-shot-world-title/">Water Boys</a> for the ICCA finals.
+					When I'm not making products, I enjoy writing, arranging, and playing music. In April 2017 I had the amazing opportunity to sing at the Beacon Theatre in New York with the <a target="_blank" href="http://www.570news.com/2017/04/05/uw-capella-group-shot-world-title/">Water Boys</a> for the ICCA finals.
 				</p>
 			</div>
 		<?php include('social.php'); ?>
