7479f9f3ace7becf56dd260767874a078b6d5f23
Adam Thompson <athompson@newvisions.org>
2019-08-12 10:16:16 -0400

    WIP - full-with portfolio cards

--- src/components/portfolio-card/portfolio-card.js
+++ src/components/portfolio-card/portfolio-card.js
@@ -8,18 +8,20 @@ import { mix } from "chroma-js";
 function getGradientColorObject(color = 'black') {
   const startColor = mix('white', color, 0.9)
   // const endColor = color;
-  // const lightColor = mix('white', color, 0.05);
+  const lightColor = mix('white', color, 0.05);
   return {
-    // color: color, /* Old browsers */
-
-    // background: lightColor, /* Old browsers */
+    /* Light */
+    color: color,
+    background: lightColor, /* Old browsers */
+    
+    /* Dark */
     /* eslint-disable */
-    background: `-moz-linear-gradient(to bottom right,  ${startColor} 0%, ${color} 100%)`, /* FF3.6+ */
-    background: `-webkit-gradient(linear, top left, bottom right, color-stop(0%, ${startColor}), color-stop(100%, ${color}))`, /* Chrome,Safari4+ */
-    background: `-webkit-linear-gradient(to bottom right,  ${startColor} 0%, ${color} 100%)`, /* Chrome10+,Safari5.1+ */
-    background: `-o-linear-gradient(to bottom right,  ${startColor} 0%, ${color} 100%)`, /* Opera 11.10+ */
-    background: `-ms-linear-gradient(to bottom right,  ${startColor} 0%, ${color} 100%)`, /* IE10+ */
-    background: `linear-gradient(to bottom right,  ${startColor} 0%, ${color} 100%)` /* W3C */
+    // background: `-moz-linear-gradient(to bottom right,  ${lightColor} 0%, ${color} 100%)`, /* FF3.6+ */
+    // background: `-webkit-gradient(linear, top left, bottom right, color-stop(0%, ${lightColor}), color-stop(100%, ${color}))`, /* Chrome,Safari4+ */
+    // background: `-webkit-linear-gradient(to bottom right,  ${lightColor} 0%, ${color} 100%)`, /* Chrome10+,Safari5.1+ */
+    // background: `-o-linear-gradient(to bottom right,  ${lightColor} 0%, ${color} 100%)`, /* Opera 11.10+ */
+    // background: `-ms-linear-gradient(to bottom right,  ${lightColor} 0%, ${color} 100%)`, /* IE10+ */
+    // background: `linear-gradient(to bottom right,  ${lightColor} 0%, ${color} 100%)` /* W3C */
   }
   
 }
@@ -27,7 +29,7 @@ function getGradientColorObject(color = 'black') {
 const PortfolioCard = ({ title, tagline, color, imgSrc, link }) => {
   return (
   <Link to={link}>
-    <Card 
+    <div 
       style={getGradientColorObject(color)}
       className="portfolio-card"
       id={title.toLowerCase()}
@@ -37,7 +39,7 @@ const PortfolioCard = ({ title, tagline, color, imgSrc, link }) => {
         <span>{tagline}</span>
       </div>
         <img src={imgSrc} alt={title}/>
-    </Card>
+    </div>
   </Link>
   )
 }
