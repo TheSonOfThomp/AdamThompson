0967832278a6873c898a6a9d31a10eecd34bd052
Adam Thompson <athompson@newvisions.org>
2019-10-12 00:56:34 -0400

    Improves styling of portfolio cards

--- src/components/portfolio-card/portfolio-card.js
+++ src/components/portfolio-card/portfolio-card.js
@@ -7,20 +7,15 @@ const PortfolioCard = ({ title, tagline, color, imgSrc, link }) => {
   const highlighted_card = 'new-visions'
   const isHighlightedCard = id === highlighted_card;
   return (
-  <Link to={link} className={`${isHighlightedCard ? 'double-wide' : ''}`}>
-    <div 
-      className={`portfolio-card clickable`}
-      id={id}
-    >
+    <Link to={link} id={id} className={`portfolio-card clickable ${isHighlightedCard ? 'double-wide' : ''}`}>
       <div className="portfolio-card-text">
         <h2>{title}</h2>
         <br/>
         <span>{tagline}</span>
       </div>
-        <div class="portfolio-card-image" style={{backgroundImage: `url(${imgSrc})`}} >
-        {/* <img src={imgSrc} alt={title}/> */}
+        <div class="portfolio-card-image using-img-tag"  >
+          <img src={imgSrc} alt={`${title}`}/>
       </div>
-    </div>
   </Link>
   )
 }
