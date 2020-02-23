daafe1ab2cec24b1ece75512a4c231f6c8d5ec90
Adam <adam.m.thompson@icloud.com>
2020-02-22 21:51:35 -0500

    Adds blog section

--- src/templates/sections/blog/blog-section.js
+++ src/templates/sections/blog/blog-section.js
@@ -1,29 +1,45 @@
 import React from "react"
-// import axios from 'axios';
 import Section from "templates/section/section";
-
-// import { useStaticQuery, graphql } from 'gatsby';
 import './blog-section.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMedium } from "@fortawesome/free-brands-svg-icons";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
-class BlogSection extends React.Component {
-  state = {
-    mediumArticles: undefined
-  }
+const posts = [
+  {
+    url: 'https://medium.com/@TheSonOfThomp/the-point-of-prototyping-4bff450dc659',
+    title: 'The Power of Prototyping',
+    date: 'Jan 2, 2020'
+  },
+  {
+    url: 'https://medium.com/@TheSonOfThomp/leaving-artifact-behind-ea0673752976',
+    title: 'Leaving Artifact Behind',
+    date: 'Oct 3, 2019'
+  }, 
+  {
+    url: 'https://medium.com/@TheSonOfThomp/the-ux-trade-off-of-faceid-b5eb2d6beed0',
+    title: 'The UX trade-off of Face ID',
+    date: 'Sep 13, 2017'
+  },
+]
 
-  componentDidMount(){
-      // const request = axios
-      // .get('https://medium.com/feed/@thesonofthomp')
-      // .then(resp => {
-      //   console.log(resp)
-      // })
-  }
-
-  render(){
-    return (
-      <Section title="Writing">
-      </Section>
-    )
-  }
+const BlogSection = () => {
+  return (
+    <Section title="Blog">
+      {posts.map(post => {
+        return (
+          <>
+          <a href={post.url} className="blog-post">
+            <h2>{post.title}</h2>
+            <span>{post.date}</span>
+            <FontAwesomeIcon icon={faExternalLinkAlt} />
+          </a>
+          <br/>
+          </>
+        )
+      })}
+    </Section>
+  )
 }
 
 export default BlogSection
