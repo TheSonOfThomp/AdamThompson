2d44682d25b76a905497853b23356e73a2a521bb
Adam Thompson <athompson@newvisions.org>
2020-04-10 13:59:50 -0400

    Improve semantics of blog post link

--- src/templates/sections/blog/BlogPostLink/BlogPostLink.js
+++ src/templates/sections/blog/BlogPostLink/BlogPostLink.js
@@ -8,11 +8,8 @@ const BlogPostLink = ({post}) => {
   const { title, url, datePublished, description } = post
   return (
     <a href={url} className="blog-post blog-post-link">
+      <h2 className="blog-post-title">{title}</h2>
       <div className="blog-post-date">{moment(datePublished).format('MMM Do YYYY')}</div>
-      <h2 className="blog-post-title">
-        {title}
-      {/* <FontAwesomeIcon icon={faExternalLinkAlt}/> */}
-      </h2>
       <p className="blog-post-description">
         {description}
       </p>
