7627f398055d5db4cb38b426ec102f903307155b
Adam Thompson <athompson@newvisions.org>
2020-07-24 16:13:05 -0400

    Adds Farkle project to array

--- src/templates/sections/projects/ProjectCard/project-card.js
+++ src/templates/sections/projects/ProjectCard/project-card.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faNpm } from '@fortawesome/free-brands-svg-icons'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
-import './project-card.styles.scss';
 import { useStaticQuery, graphql } from 'gatsby';
+import './project-card.styles.scss';
+
+import { useCustomProp } from  '../../../../hooks/useCustomProperty'
 
 const ProjectCard = ({project}) => {
   const iconObj = 
@@ -25,15 +27,31 @@ const ProjectCard = ({project}) => {
     }
   `)
 
-  const logoSrc = imgQuery.allImageSharp.nodes.find(node => node.fluid.originalName === project.logo).fluid.src
+  const logoSrc = imgQuery.allImageSharp.nodes.find(node => node.fluid.originalName === project.logo)?.fluid.src
+
+  const cardRef = useCustomProp('--project-color', project.color)
 
   return (
-  <a className="project-card" href={project.url} alt={project.name}>
-    <h2 className="project-title">{project.name}</h2>
-    <span className="project-description">{project.description}</span>
-    <img className="project-logo" src={logoSrc}></img>
-    <FontAwesomeIcon className="project-icon" icon={iconObj} size="sm" />
-  </a>
+    <a ref={cardRef} className="project-card" href={project.url} alt={project.name}>
+      <h2 className="project-title">{project.name}</h2>
+      <span className="project-description">{project.description}</span>
+      <img className="project-logo" src={logoSrc} alt={`Logo for ${project.name}`}></img>
+      <FontAwesomeIcon className="project-icon" icon={iconObj} size="sm" />
+      {
+        project.tools && (
+          <div className="project-tools-wrapper">
+            <div className="project-tools-label">Built with</div>
+            <div className="project-tools-list">
+              {
+                project.tools.map(tool => (
+                  <span className="project-tool">{tool}</span>
+                ))
+              }
+            </div>
+          </div>
+        )
+      }
+    </a>
 )};
 
 export default ProjectCard;
