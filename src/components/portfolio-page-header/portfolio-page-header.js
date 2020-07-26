bb446940c38675be71d124ce1f07940f7e51a471
Adam Thompson <athompson@newvisions.org>
2020-07-26 15:41:33 -0400

    Clean up portfolio page

--- src/components/portfolio-page-header/portfolio-page-header.js
+++ src/components/portfolio-page-header/portfolio-page-header.js
@@ -1,9 +1,9 @@
 import React from "react"
 import monogram from '../../images/monogram-black.svg';
-import './porfolio-page-header.scss'
+import './portfolio-page-header.scss'
 
 export const PortfolioPageHeader = () => (
-  <header className="porfolio-page-header">
+  <header className="portfolio-page-header">
     <a href="/">
       <img src={monogram} alt="A.T. monogram"/>
       <h1>Adam Thompson</h1>
--- src/hooks/useCustomProperty.js
+++ src/hooks/useCustomProperty.js
@@ -14,3 +14,22 @@ export const useCustomProp = (propName, value) => {
 
   return ref
 }
+
+// Same as above, but for setting multiple properties at once
+// type customPropsObject = {[key:string]: string};
+export const useCustomProps = (customProperties) => {
+  const ref = useRef();
+  useEffect(() => {
+    if (ref) {
+      if (typeof customProperties === 'object') {
+        Object.entries(customProperties).forEach(prop => {
+          ref.current.style.setProperty(prop[0], prop[1])
+        })
+      }
+    } else {
+      console.warn(`Unused ref returned by useCustomProp`)
+    }
+  }, [customProperties, ref])
+
+  return ref
+}
