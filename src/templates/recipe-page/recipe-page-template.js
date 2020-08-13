fa3f2e1f09363ea71db713eee4949cee215d23b6
Adam Thompson <athompson@newvisions.org>
2020-08-12 20:09:18 -0400

    Updates recipe page

--- src/templates/recipe-page/recipe-page-template.js
+++ src/templates/recipe-page/recipe-page-template.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { PortfolioPageHeader } from "../../components/portfolio-page-header/portfolio-page-header";
 import './recipe-page-template.styles.scss';
 
 const RecipePage = ({children}) => {
   return (
-    <div className="recipe-page-wrapper">
-      {children}
-    </div>
+    <main className="recipe-page">
+      <PortfolioPageHeader />
+      <article>
+        {children}
+      </article>
+    </main>
   );
 }
 
