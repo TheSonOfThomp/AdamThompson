a701b60809e1037dae61c7692f404ac0212bd4ad
Adam Thompson <athompson@newvisions.org>
2020-09-02 23:59:37 -0400

    Fix vh issue

--- src/hooks/useVH.js
+++ src/hooks/useVH.js
@@ -1,7 +1,17 @@
+import { useEffect, useCallback } from "react"
+import { useWindowSize } from "./useWindowSize"
+
 export const useVH = () => {
+  const { height } = useWindowSize()
+
+  const resetVH = useCallback(() => {
+    const vh = height / 100
+    document.body.style.setProperty('--vh', `${vh}px`)
+  }, [height])
+
   useEffect(() => {
-    window.addEventListener('resize', () => {
-      document.body.style.setProperty('--vh', `${window.innerHeight / 100}px`)
-    })
-  }, [])
+    resetVH()
+    window.addEventListener('resize', resetVH)
+    return window.removeEventListener('resize', resetVH)
+  }, [height, resetVH])
 }
