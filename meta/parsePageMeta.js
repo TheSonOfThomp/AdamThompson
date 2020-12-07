f7617eb5c1f722fe4bb66caa753ffb72a041d7af
Adam Thompson <athompson@newvisions.org>
2020-12-06 22:10:13 -0500

    Stubs pad thai

--- meta/parsePageMeta.js
+++ meta/parsePageMeta.js
@@ -5,6 +5,8 @@ const getVarName = (file) => file.replace(/[-.]/g, '')
 
 for (let dir of subdirectories) {
   fs.readdir(`./pages/${dir}`, (err, files) => {
+    files = files.filter(file => !file.startsWith('_'))
+
     const imports = files.map(file => {
       return `
         const ${getVarName(file)} = require('../pages/${dir}/${file}').meta
