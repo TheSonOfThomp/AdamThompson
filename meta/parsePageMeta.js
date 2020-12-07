7a2c0af01709a8cfe51845c6d43ffe38d0978819
Adam Thompson <athompson@newvisions.org>
2020-12-06 22:00:10 -0500

    update prestart & add mcsorley's chili

--- meta/parsePageMeta.js
+++ meta/parsePageMeta.js
@@ -6,8 +6,11 @@ const getVarName = (file) => file.replace(/[-.]/g, '')
 for (let dir of subdirectories) {
   fs.readdir(`./pages/${dir}`, (err, files) => {
     const imports = files.map(file => {
-      return `const ${getVarName(file)} = require('../pages/${dir}/${file}').meta`
-    }).join('\n')
+      return `
+        const ${getVarName(file)} = require('../pages/${dir}/${file}').meta
+        ${getVarName(file)}.slug = "${file.replace('.mdx', '')}"
+      `
+    }).join('')
     const exports = `\nexport const ${dir} = [${files.map(getVarName).join(',')}]`
     fs.writeFileSync(`${__dirname}/${dir}-meta.js`, imports+exports)
   })
--- meta/portfolio-meta.js
+++ meta/portfolio-meta.js
@@ -1,4 +1,8 @@
 const chameleonmdx = require('../pages/portfolio/chameleon.mdx').meta
-const newvisionsmdx = require('../pages/portfolio/newvisions.mdx').meta
-const noommdx = require('../pages/portfolio/noom.mdx').meta
+      chameleonmdx.slug = "chameleon"
+      const newvisionsmdx = require('../pages/portfolio/newvisions.mdx').meta
+      newvisionsmdx.slug = "newvisions"
+      const noommdx = require('../pages/portfolio/noom.mdx').meta
+      noommdx.slug = "noom"
+      
 export const portfolio = [chameleonmdx,newvisionsmdx,noommdx]
