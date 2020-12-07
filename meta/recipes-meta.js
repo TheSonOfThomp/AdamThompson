153c0d9e256bf050121156768e3ba73509184122
Adam Thompson <athompson@newvisions.org>
2020-12-06 19:12:35 -0500

    jambalaya

--- meta/recipes-meta.js
+++ meta/recipes-meta.js
@@ -1,6 +1,8 @@
 const bangbangshrimpmdx = require('../pages/recipes/bang-bang-shrimp.mdx').meta
+const cajunseasoningmdx = require('../pages/recipes/cajun-seasoning.mdx').meta
 const chickentikkamasalamdx = require('../pages/recipes/chicken-tikka-masala.mdx').meta
 const falafelmdx = require('../pages/recipes/falafel.mdx').meta
+const jambalayamdx = require('../pages/recipes/jambalaya.mdx').meta
 const spicypeanuttofumdx = require('../pages/recipes/spicy-peanut-tofu.mdx').meta
 const tabboulehmdx = require('../pages/recipes/tabbouleh.mdx').meta
-export const recipes = [bangbangshrimpmdx,chickentikkamasalamdx,falafelmdx,spicypeanuttofumdx,tabboulehmdx]
+export const recipes = [bangbangshrimpmdx,cajunseasoningmdx,chickentikkamasalamdx,falafelmdx,jambalayamdx,spicypeanuttofumdx,tabboulehmdx]
