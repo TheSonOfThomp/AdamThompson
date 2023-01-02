b1521b9c47a0dfc44076e960769445d61b6089fe
Adam Thompson <2414030+TheSonOfThomp@users.noreply.github.com>
2023-01-01 22:02:36 -0500

    Update footer-section.js

--- src/main-sections/footer/footer-section.js
+++ src/main-sections/footer/footer-section.js
@@ -1,16 +1,12 @@
 import React from "react"
 import styles from './footer-section.module.scss';
 
-const FooterSection = () => {
-  const now = new Date();
-  const year = now.getFullYear();
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.footer_content}>
-        <span>© {year} Adam Thompson. Built with Next.js</span>
-      </div>
-    </footer>
-  )
-}
+const FooterSection = () => (
+  <footer className={styles.footer}>
+    <div className={styles.footer_content}>
+      <span>© Adam Thompson. Built with Next.js</span>
+    </div>
+  </footer>
+)
 
 export default FooterSection
