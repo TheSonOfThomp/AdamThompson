36bf8339735af92d8c0da26fec6bc9ae54903563
Adam Thompson <adam.m.thompson@icloud.com>
2024-01-06 00:12:48 -0500

    sb sass codemod

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,49 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    ({
+        name: "@storybook/addon-styling-webpack",
+
+        options: {
+          rules: [{
+        test: /\.css$/,
+        sideEffects: true,
+        use: [
+            require.resolve("style-loader"),
+            {
+                loader: require.resolve("css-loader"),
+                options: {
+                    
+                    
+                },
+            },
+        ],
+      },{
+        test: /\.s[ac]ss$/,
+        sideEffects: true,
+        use: [
+            require.resolve("style-loader"),
+            {
+                loader: require.resolve("css-loader"),
+                options: {
+                    
+                    importLoaders: 2,
+                },
+            },
+            require.resolve("resolve-url-loader"),
+            {
+                loader: require.resolve("sass-loader"),
+                options: {
+                    // Want to add more Sass options? Read more here: https://webpack.js.org/loaders/sass-loader/#options
+                    implementation: require.resolve("sass"),
+                    sourceMap: true,
+                    sassOptions: {},
+                },
+            },
+        ],
+      },],
+        }
+    })
   ],
   framework: {
     name: "@storybook/nextjs",
